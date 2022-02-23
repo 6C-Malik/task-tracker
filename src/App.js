cce1f88fa0c7884267562199dafbbc10c6de90a9
6C-Malik <cheickito@hotmail.com>
2022-02-23 15:50:09 +0100

    task tracker first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Tasks from "./components/Tasks";
+import AddTask from "./components/AddTask";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: "Rendez-vous chez le docteur",
+      day: "2 Janvier à 14h30",
+      reminder: false,
+    },
+    {
+      id: 2,
+      text: "Rendez-vous à l'école",
+      day: "19 Janvier à 9h30",
+      reminder: false,
+    },
+    {
+      id: 3,
+      text: "Rendez-vous à Pôle emploi",
+      day: "15 Juillet à 10h00",
+      reminder: true,
+    },
+    {
+      id: 4,
+      text: "Rendez-vous à la gare du nord",
+      day: "7 Août à 14h30",
+      reminder: true,
+    },
+  ]);
+
+  //Add Task
+  const addTask = (task) => {
+    const id = Math.floor(Math.random() * 10000) +1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
+  // Delete Task
+  const deleteTask = (id) => {
+    setTasks(tasks.filter((task) => task.id !== id));
+  };
+
+  //Toggle Reminder
+  const toggleReminder = (id) => {
+    setTasks(
+      tasks.map((task) =>
+        task.id === id ? { ...task, reminder: !task.reminder } : task
+      )
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <AddTask onAdd={addTask}/>
+      {tasks.length > 0 ? (
+        <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
+      ) : (
+        "No Tasks to show"
+      )}
     </div>
   );
 }
-
-export default App;
---
+++ src/components/AddTask.js
@@ -0,0 +1,43 @@
+import { useState } from "react"
+
+export default function AddTask({ onAdd }) {
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if(!text) {
+            alert('Entrer une tâche s\'il vous plait')
+            return
+        }
+
+        onAdd({ text, day, reminder })
+
+        setText('')
+        setDay('')
+        setReminder(false)
+
+
+    }
+
+  return (
+    <form className='add-form' onSubmit={onSubmit}>
+        <div className="form-control">
+            <label>task</label>
+            <input type='text' placeholder='Ajouter tâche' value={text} onChange={(e) => setText(e.target.value)} />
+        </div>
+        <div className="form-control">
+            <label>Jour et Heure</label>
+            <input type='text' placeholder='Ajouter Date & heure' value={day} onChange={(e) => setDay(e.target.value)}/>
+        </div>
+        <div className="form-control form-control-check">
+            <label>Rappel</label>
+            <input type='checkbox' value={reminder} checked={reminder} onChange={(e) => setReminder(e.currentTarget.checked)}/>
+        </div>
+
+        <input type='submit' value='Enregistrer tâche' className='btn btn-block'/>
+    </form>
+  )
+}
